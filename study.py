82f44973fe1d359b255f78beebb99012a34ffb19
smithc57973 <44327733+smithc57973@users.noreply.github.com>
2020-11-30 14:58:02 -0600

    Update study.py

--- study.py
+++ study.py
@@ -1,11 +1,25 @@
+###
+# Study Buddy
+# Coursebuddy Prototype
+# A basic prototype for the Coursebuddy system using surprise.
+###
 
+import numpy as np
+import pandas as pd
+import surprise as surprise
 from surprise import SVD
 from surprise import Dataset
-from surprise.model_selection import cross_validat
+from surprise import Reader
+from surprise.model_selection import cross_validate
 
-data = Dataset.load_builtin('ml-100k')
+df = pd.read_csv("Prototype Data - Sheet1.csv")
+print(df)
 
 algo = SVD()
-
-cross_validate(algo, data, measures = ['RMSE', 'MAE'], cv=5, verbose=True)
-
+rd = {"Course" : ["CS 100", "CS 101", "CS 102", "CS 103"],
+      "User" : ["Bob", "Sally", "Tim", "Samantha"],
+      "FinalGrade" : [99, 88, 77, 66]}
+df = pd.DataFrame(rd)
+reader = Reader(rating_scale = (0, 100))
+data = Dataset.load_from_df(df[["User", "Course", "FinalGrade"]], reader)
+cross_validate(algo, data, measures = ['RMSE', 'MAE'], cv=4, verbose=True)
