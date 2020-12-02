2681e344526bbd722449825466b15695d28b3f8a
smithc57973 <44327733+smithc57973@users.noreply.github.com>
2020-12-01 18:55:05 -0600

    Update study.py

--- study.py
+++ study.py
@@ -23,3 +23,43 @@ df = pd.DataFrame(rd)
 reader = Reader(rating_scale = (0, 100))
 data = Dataset.load_from_df(df[["User", "Course", "FinalGrade"]], reader)
 cross_validate(algo, data, measures = ['RMSE', 'MAE'], cv=4, verbose=True)
+
+from collections import defaultdict
+def get_top_n(predictions, n=10):
+    """Return the top-N recommendation for each user from a set of predictions.
+
+    Args:
+        predictions(list of Prediction objects): The list of predictions, as
+            returned by the test method of an algorithm.
+        n(int): The number of recommendation to output for each user. Default
+            is 10.
+
+    Returns:
+    A dict where keys are user (raw) ids and values are lists of tuples:
+        [(raw item id, rating estimation), ...] of size n.
+    """
+
+    # First map the predictions to each user.
+    top_n = defaultdict(list)
+    for uid, iid, true_r, est, _ in predictions:
+        top_n[uid].append((iid, est))
+
+    # Then sort the predictions for each user and retrieve the k highest ones.
+    for uid, user_ratings in top_n.items():
+        user_ratings.sort(key=lambda x: x[1], reverse=True)
+        top_n[uid] = user_ratings[:n]
+
+    return top_n
+	
+	trainset = data.build_full_trainset()
+testset = trainset.build_anti_testset()
+preds = algo.test(testset)
+top = get_top_n(preds, n=10)
+print(top)
+
+for key, value in top.items():
+    print(key)
+    for i in range(len(value)):
+        print(value[i])
+		
+		
