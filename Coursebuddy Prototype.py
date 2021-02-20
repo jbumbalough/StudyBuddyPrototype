4e653f14aa376c97f53b7209090744b780f6a4f8
smithc57973 <44327733+smithc57973@users.noreply.github.com>
2021-02-20 11:17:45 -0600

    Update Coursebuddy Prototype.py

--- Coursebuddy Prototype.py
+++ Coursebuddy Prototype.py
@@ -17,6 +17,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split, cross_val_score, KFold
 from sklearn.neighbors import KNeighborsClassifier
 
+##Getting the data
 df = pd.read_csv("CourseBuddyDATA_SAMPLE1.csv")
 df
 scaler = StandardScaler()
@@ -25,6 +26,7 @@ scaler = StandardScaler()
 # In[2]:
 
 
+##Transforming the data
 dfr = df.replace('A', 5)
 dfr = dfr.replace('B', 4)
 dfr = dfr.replace('C', 3)
@@ -36,90 +38,110 @@ dfr
 # In[3]:
 
 
+##Predicting final grade, based on all other grades for all students
 x_train, x_test, y_train, y_test = train_test_split(dfr.drop(['S#', 'Course Grade'], axis=1), dfr['Course Grade'])
 
 
 # In[4]:
 
 
-x_train
+#x_train #x_test #y_train #y_test
 
 
 # In[5]:
 
 
-x_test
+##Finding the best k neighbors
+fe = []
+for i in range(1, 13):
+    knn = KNeighborsClassifier(n_neighbors = i, p = 2)
+    knn.fit(x_train, y_train)
+    pred = knn.predict(x_test)
+    fe.append(np.mean(cross_val_score(knn, x_test, y_test, cv=5)))
 
 
 # In[6]:
 
 
-y_train
+fe
 
 
 # In[7]:
 
 
-y_test
+##The Prediction
+k_star = np.argmin(fe)
+print(k_star)
+knn = KNeighborsClassifier(n_neighbors = k_star, p = 2)
+knn.fit(x_train, y_train)
+pred = knn.predict(x_test)
+print(pred)
+knn.score(x_test, y_test)*100
 
 
 # In[8]:
 
 
-fe = []
-for i in range(1, 13):
-    knn = KNeighborsClassifier(n_neighbors = i, p = 2)
+##Predicting each of the grade items, based on all other grade items for all students
+features = ['Class Participation [100]', 'HW#1 [100]', 'HW#2 [100]', 'HW#3 [100]', 'Test#1 [100]', 'Test#2 [100]', 'Test#3 [100]', 'FinalExam [300]', 'Course Grade']
+
+for i in features:
+    print(i)
+    x_train, x_test, y_train, y_test = train_test_split(dfr.drop(['S#', i], axis=1), dfr[i])
+    knn = KNeighborsClassifier(n_neighbors = 5, p = 2)
     knn.fit(x_train, y_train)
     pred = knn.predict(x_test)
-    fe.append(np.mean(cross_val_score(knn, x_test, y_test, cv=5)))
+    print(y_test)
+    print(pred)
+    print(knn.score(x_test, y_test)*100)
 
 
-# In[9]:
+# In[22]:
 
 
-fe
+##Predicting each of the grade items over time, based on available previous data
+#Todo
+a_std = pd.DataFrame({'S#' : [100], 'Prerequisite 1': [5], 'Prerequisite 2': [5], 'Prerequisite 3': [5]})
+print(a_std)
+
+for i in features:
+    pred = knn.predict(a_std)
+    print(pred)
+    #print(knn.score(x_test, y_test)*100)
+
+#dfr.append(a_std, ignore_index=True)
+#print(dfr)
 
 
 # In[10]:
 
 
-k_star = np.argmin(fe)
-print(k_star)
-knn = KNeighborsClassifier(n_neighbors = k_star, p = 2)
-knn.fit(x_train, y_train)
-pred = knn.predict(x_test)
-knn.score(x_test, y_test)*100
+###
+#features = [['Prerequisite 1', 'Prerequisite 2', 'Prerequisite 3', 'Class Participation [100]', 'HW#1 [100]', 'HW#2 [100]', 'HW#3 [100]', 'Test#1 [100]', 'Test#2 [100]', 'Test#3 [100]', 'FinalExam [300]']]
+#for feature in features:
+#    dfr[feature] = scaler.fit_transform(dfr[feature])
+
+#x_train, x_test, y_train, y_test = train_test_split(dfr.drop(['S#', 'Course Grade'], axis=1), dfr['Course Grade'])
 
 
 # In[11]:
 
 
-################################################
-features = [['Prerequisite 1', 'Prerequisite 2', 'Prerequisite 3', 'Class Participation [100]', 'HW#1 [100]', 'HW#2 [100]', 'HW#3 [100]', 'Test#1 [100]', 'Test#2 [100]', 'Test#3 [100]', 'FinalExam [300]']]
-for feature in features:
-    dfr[feature] = scaler.fit_transform(dfr[feature])
-
-x_train, x_test, y_train, y_test = train_test_split(dfr.drop(['S#', 'Course Grade'], axis=1), dfr['Course Grade'])
+#fe = []
+#for i in range(1, 13):
+#    knn = KNeighborsClassifier(n_neighbors = i, p = 2)
+#    knn.fit(x_train, y_train)
+#    pred = knn.predict(x_test)
+#    fe.append(np.mean(cross_val_score(knn, x_test, y_test, cv=5)))
 
 
 # In[12]:
 
 
-fe = []
-for i in range(1, 13):
-    knn = KNeighborsClassifier(n_neighbors = i, p = 2)
-    knn.fit(x_train, y_train)
-    pred = knn.predict(x_test)
-    fe.append(np.mean(cross_val_score(knn, x_test, y_test, cv=5)))
-
-
-# In[13]:
-
-
-k_star = np.argmin(fe)
-print(k_star)
-knn = KNeighborsClassifier(n_neighbors = k_star, p = 2)
-knn.fit(x_train, y_train)
-pred = knn.predict(x_test)
-knn.score(x_test, y_test)*100
+#k_star = np.argmin(fe)
+#print(k_star)
+#knn = KNeighborsClassifier(n_neighbors = k_star, p = 2)
+#knn.fit(x_train, y_train)
+#pred = knn.predict(x_test)
+#knn.score(x_test, y_test)*100
 
